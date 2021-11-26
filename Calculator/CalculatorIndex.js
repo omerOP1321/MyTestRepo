7d5c5b3d497923125d876440f877212a3efecb68
omerOP <gnr1321.op@gmail.com>
2021-11-27 00:25:23 +0200

    Working claculator with eval()

---
+++ Calculator/CalculatorIndex.js
@@ -0,0 +1,33 @@
+//getting the numbers on the screen and in a var
+const numberButtons = document.querySelectorAll('.numbers');
+const opratorButtons = document.querySelectorAll('.operator');
+const decimalButton = document.querySelector('.decimal');
+const acButton = document.querySelector('.all-clear');
+const eqButton = document.querySelector('.equal-sign');
+const calcScreen = document.querySelector('.calculator-screen');
+let equation = 0;
+//intalazing the operation
+numberButtons.forEach((button) => button.addEventListener('click', (e) => {
+    calcScreen.value += e.target.value;
+    equation = calcScreen.value;
+}));
+opratorButtons.forEach((button) => button.addEventListener('click', (e) => {
+    calcScreen.value += e.target.value;
+    equation = calcScreen.value;
+}));
+
+decimalButton.addEventListener('click', (e) => {
+    calcScreen.value += e.target.value;
+    equation = calcScreen.value;
+})
+
+acButton.addEventListener('click', (e) => {
+    calcScreen.value = '';
+    equation = '';
+})
+
+eqButton.addEventListener('click', (e) => {
+    calcScreen.value = eval(equation);
+    equation = '';
+
+})
