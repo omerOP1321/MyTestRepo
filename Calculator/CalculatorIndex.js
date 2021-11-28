f97729bb5b6292ce1183d8448d6d5ed5765dc2a4
omerOP1321 <gnr1321.op@gmail.com>
2021-11-28 13:01:15 +0200

    Fixed operator after operator

--- Calculator/CalculatorIndex.js
+++ Calculator/CalculatorIndex.js
@@ -1,33 +1,43 @@
 //getting the numbers on the screen and in a var
-const numberButtons = document.querySelectorAll('.numbers');
-const opratorButtons = document.querySelectorAll('.operator');
-const decimalButton = document.querySelector('.decimal');
-const acButton = document.querySelector('.all-clear');
-const eqButton = document.querySelector('.equal-sign');
-const calcScreen = document.querySelector('.calculator-screen');
-let equation = 0;
-//intalazing the operation
-numberButtons.forEach((button) => button.addEventListener('click', (e) => {
-    calcScreen.value += e.target.value;
-    equation = calcScreen.value;
-}));
-opratorButtons.forEach((button) => button.addEventListener('click', (e) => {
-    calcScreen.value += e.target.value;
-    equation = calcScreen.value;
-}));
+const numberButtons = document.querySelectorAll(".numbers")
+const opratorButtons = document.querySelectorAll(".operator")
+const decimalButton = document.querySelector(".decimal")
+const acButton = document.querySelector(".all-clear")
+const eqButton = document.querySelector(".equal-sign")
+const calcScreen = document.querySelector(".calculator-screen")
+let equation = ""
+    //intalazing the operation
+numberButtons.forEach((button) =>
+    button.addEventListener("click", (e) => {
+        calcScreen.value += e.target.value
+        equation = calcScreen.value
+    })
+)
+opratorButtons.forEach((button) =>
+    button.addEventListener("click", (e) => {
+        if (
+            equation[equation.length - 1] !== "/" &&
+            equation[equation.length - 1] !== "*" &&
+            equation[equation.length - 1] !== "-" &&
+            equation[equation.length - 1] !== "+"
+        ) {
+            calcScreen.value += e.target.value
+            equation = calcScreen.value
+        }
+    })
+)
 
-decimalButton.addEventListener('click', (e) => {
-    calcScreen.value += e.target.value;
-    equation = calcScreen.value;
+decimalButton.addEventListener("click", (e) => {
+    calcScreen.value += e.target.value
+    equation = calcScreen.value
 })
 
-acButton.addEventListener('click', (e) => {
-    calcScreen.value = '';
-    equation = '';
+acButton.addEventListener("click", (e) => {
+    calcScreen.value = ""
+    equation = ""
 })
 
-eqButton.addEventListener('click', (e) => {
-    calcScreen.value = eval(equation);
-    equation = '';
-
+eqButton.addEventListener("click", (e) => {
+    calcScreen.value = eval(equation)
+    equation = ""
 })
